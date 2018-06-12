qt/qtbase

Add the ability to enable various SSL bug workarounds.
There are lots of buggy SSL servers around and to connect to them you
need to disable various features. This commit adds the ability to
disable the SSL ticket extension, the ability to disable the insertion
of empty fragments, and the ability to disable compression.

Task-number: QTBUG-21906

Change-Id: I3e1d0347a46e9030b889bbf15b2aad19b8513b73
Merge-request: 68
Reviewed-by: Peter Hartmann <peter.hartmann@nokia.com>

tests/manual/qssloptions/main.cpp
@@ -1,0 +1,92 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtNetwork/qsslconfiguration.h>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QTextStream>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+
+    if (argc < 3) {
+        QTextStream out(stdout);
+        out << "Usage: " << argv[0] << " host port [options]" << endl;
+        out << "The options can be one or more of the following:" << endl;
+        out << "enable_empty_fragments" << endl;
+        out << "disable_session_tickets" << endl;
+        out << "disable_compression" << endl;
+        out << "disable_sni" << endl;
+        return 1;
+    }
+
+    QString host = QString::fromLocal8Bit(argv[1]);
+    int port = QString::fromLocal8Bit(argv[2]).toInt();
+
+    QSslConfiguration config = QSslConfiguration::defaultConfiguration();
+
+    for (int i=3; i < argc; i++) {
+        QString option = QString::fromLocal8Bit(argv[i]);
+
+        if (option == QStringLiteral("enable_empty_fragments"))
+            config.setSslOption(QSsl::SslOptionDisableEmptyFragments, false);
+        else if (option == QStringLiteral("disable_session_tickets"))
+            config.setSslOption(QSsl::SslOptionDisableSessionTickets, true);
+        else if (option == QStringLiteral("disable_compression"))
+            config.setSslOption(QSsl::SslOptionDisableCompression, true);
+        else if (option == QStringLiteral("disable_sni"))
+            config.setSslOption(QSsl::SslOptionDisableServerNameIndication, true);
+    }
+
+    QSslConfiguration::setDefaultConfiguration(config);
+
+    QSslSocket socket;
+    //socket.setSslConfiguration(config);
+    socket.connectToHostEncrypted(host, port);
+
+    if ( !socket.waitForEncrypted() ) {
+        qDebug() << socket.errorString();
+        return 1;
+    }
+
+    return 0;
+}

community-ssu/qt-x11-maemo

(cherry picked from commit 78d02e93aca5325fc5be9bfd275862795207abaa)
(commit was cherry-picked from Qt 5 to 4.8 after agreeing with the
author because the merge request was filed against Qt 5.)

tests/manual/qssloptions/main.cpp
@@ -1,0 +1,92 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtNetwork/qsslconfiguration.h>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QTextStream>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+
+    if (argc < 3) {
+        QTextStream out(stdout);
+        out << "Usage: " << argv[0] << " host port [options]" << endl;
+        out << "The options can be one or more of the following:" << endl;
+        out << "enable_empty_fragments" << endl;
+        out << "disable_session_tickets" << endl;
+        out << "disable_compression" << endl;
+        out << "disable_sni" << endl;
+        return 1;
+    }
+
+    QString host = QString::fromLocal8Bit(argv[1]);
+    int port = QString::fromLocal8Bit(argv[2]).toInt();
+
+    QSslConfiguration config = QSslConfiguration::defaultConfiguration();
+
+    for (int i=3; i < argc; i++) {
+        QString option = QString::fromLocal8Bit(argv[i]);
+
+        if (option == QLatin1String("enable_empty_fragments"))
+            config.setSslOption(QSsl::SslOptionDisableEmptyFragments, false);
+        else if (option == QLatin1String("disable_session_tickets"))
+            config.setSslOption(QSsl::SslOptionDisableSessionTickets, true);
+        else if (option == QLatin1String("disable_compression"))
+            config.setSslOption(QSsl::SslOptionDisableCompression, true);
+        else if (option == QLatin1String("disable_sni"))
+            config.setSslOption(QSsl::SslOptionDisableServerNameIndication, true);
+    }
+
+    QSslConfiguration::setDefaultConfiguration(config);
+
+    QSslSocket socket;
+    //socket.setSslConfiguration(config);
+    socket.connectToHostEncrypted(host, port);
+
+    if ( !socket.waitForEncrypted() ) {
+        qDebug() << socket.errorString();
+        return 1;
+    }
+
+    return 0;
+}